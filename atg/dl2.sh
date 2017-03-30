e989e8116cff8c738e4c812a4084012472f89f75
Gilles Taddei <dev@teerex.ovh>
2017-03-30 23:14:11 +0200

    adding Against The Gods

---
+++ atg/dl2.sh
@@ -0,0 +1,22 @@
+mkdir -p input
+
+function dl {
+    if ! [ -f ./input/atg-chapter-$1 ]; then
+	wget http://www.wuxiaworld.com/atg-index/atg-chapter-$1 -O input/atg-chapter-$1;
+    fi
+}
+
+    i=1;
+    dl $i
+    res=$?
+    while [ $res -eq 0 ]; do
+	i=$((i+1))
+	dl $i
+	res=$?
+	if [ $res -ne 0 ]; then
+	    rm -f input/atg-chapter-$i
+
+	fi
+    done
+
+#for i in `seq 1 280`; do echo $i; done
